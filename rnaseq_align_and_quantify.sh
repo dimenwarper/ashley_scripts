2ef1bb2c0d881562a0046e1782b076b9aa7fdc66
Tsuname Tendou <dimenwarper@gmail.com>
2014-06-25 17:32:00 -0700

    frist commit

---
+++ rnaseq_align_and_quantify.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Running pipeline for $1"
+cd $1/
+echo "In directory"
+pwd
+/home/STAR/STAR --genomeDir $2/Sequence/StarFiles/ --readFilesIn R1.fq R2.fq --runThreadN 8
+echo "Sorting alignment"
+sed 's/chrM/chrZ/g' Aligned.out.sam | sort -k 3,3 -k 4,4n > Aligned.out.sorted.sam
+/home/cufflinks/cufflinks --library-type fr-firststrand --GTF-guide $2/Annotation/Genes/genes.gtf Aligned.out.sorted.sam
