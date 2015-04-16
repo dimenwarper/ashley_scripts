36ddab55d35adb84d295db523612d4b5729cf882
Pablo Cordero <dimenwarper@gmail.com>
2015-04-16 18:27:50 +0000

    Mofied rnaseq_align_and_quantify to make it a bit more general

--- rnaseq_align_and_quantify.sh
+++ rnaseq_align_and_quantify.sh
@@ -3,7 +3,7 @@ echo "Running pipeline for $1"
 cd $1/
 echo "In directory"
 pwd
-/home/STAR/STAR --genomeDir $2/Sequence/StarFiles/ --readFilesIn R1.fq R2.fq --runThreadN 8
+/home/STAR/STAR --genomeDir $2/StarFiles/ --readFilesIn <(cat *R1*) <(cat *R2*) --runThreadN 8
 echo "Sorting alignment"
 sed 's/chrM/chrZ/g' Aligned.out.sam | sort -k 3,3 -k 4,4n > Aligned.out.sorted.sam
 /home/cufflinks/cuffquant --library-type fr-firststrand -p 4 $2/Annotation/Genes/genes.gtf Aligned.out.sorted.sam
